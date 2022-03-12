cf12006b266d861e1825d8c4bcc9ebef20505d79
SKwambua <samuelkikuvi33@gmail.com>
2022-03-12 22:46:25 +0000

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main-entry point
+ *
+ * return:always 0 (success)
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
-
+	puts("\"Programming is like building a muitilingual puzzle");
+	return (0);
 }
-
