4b787e1240e1b1826309a537b14b5e0f597afc0a
SKwambua <samuelkikuvi33@gmail.com>
2022-03-15 01:14:56 +0000

    use of operator NOT

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z';)
+	{
+		if (b != 'e' && b != 'q')
+		putchar(b);
+		putchar('\n');
+	}
+	return (0);
+}
+
