a9a478f9b59336bf5eb0eb1fd07301dda94d5752
SKwambua <samuelkikuvi33@gmail.com>
2022-03-18 21:23:44 +0000

    change

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * more_numbers - function to print 10 times the number, from 0 to 14,
+ * followed by a new line
+ * 
+ * Return: Always 0 (success)
+ */
+void more_numbers(void)
+{
