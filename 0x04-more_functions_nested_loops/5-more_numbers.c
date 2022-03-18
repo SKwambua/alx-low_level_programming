d2c4c3c9ffd73df394df4d96d45250811ddc1ec7
SKwambua <samuelkikuvi33@gmail.com>
2022-03-18 22:23:18 +0000

    print numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,8 +2,23 @@
 /**
  * more_numbers - function to print 10 times the number, from 0 to 14,
  * followed by a new line
- * 
+ *
  * Return: Always 0 (success)
  */
 void more_numbers(void)
 {
+	int a, b;
+
+	for (a = 0; b < 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+				_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
+
+
