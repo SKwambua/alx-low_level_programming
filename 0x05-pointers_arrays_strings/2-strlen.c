1d390498aef736295c438cd9dbe240a047d4ec1c
SKwambua <samuelkikuvi33@gmail.com>
2022-03-22 00:25:45 +0000

    return the length of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - retuns the length of a string
+ * @s: string to evaluate
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
