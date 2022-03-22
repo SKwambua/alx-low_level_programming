39f16d18c9b239dc217a12ad40a05e25804427d3
SKwambua <samuelkikuvi33@gmail.com>
2022-03-22 01:18:35 +0000

    convert strings to integers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string, starting with first character,
+ * followed by a new line
+ * @str: is the string to print characters from
+ */
+void puts2(char *str)
+{
+
+}
