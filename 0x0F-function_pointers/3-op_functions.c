e67a7f187fdcb93415ba1928b0bcd08ad877b61d
SKwambua <samuelkikuvi33@gmail.com>
2022-04-13 23:21:44 +0000

    funtions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - adds
+ * @a: arugument
+ * @b: argurment
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - returns the difference
+ * @a: argument
+ * @b: argument
+ * Return: difference between a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - product of a and b
+ * @a: arguments
+ * @b: argument
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division of a and b
+ * @a: argument
+ * @b: argument
+ * Return: division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - calculate the remainder
+ * @a: argument
+ * @b: argument
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
