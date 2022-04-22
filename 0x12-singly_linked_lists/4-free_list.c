36c7d4ac0fb7e67e54845ed4dfc4ad71def6a745
SKwambua <samuelkikuvi33@gmail.com>
2022-04-22 01:25:15 +0000

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * free_list - free a struct linked list
+ * @head: first element
+ * Return: frees a list_t list
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+	free_list(head->next);
+	free(head->str);
+	free(head);
+}
