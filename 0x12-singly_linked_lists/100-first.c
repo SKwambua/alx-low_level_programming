8d7ee912340c00d2b2010371fa9f2f9ad2157389
SKwambua <samuelkikuvi33@gmail.com>
2022-04-22 01:40:10 +0000

    print strings

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,7 +8,7 @@ void _construct(void) __attribute__((constructor));
 void _construct(void)
 {
 	char line1[] = "You're beat! and yet, you must allow,";
-	char line2[] = "I bore my house upon my my back!";
+	char line2[] = "I bore my house upon my back!";
 
 	printf("%s\n%s\n", line1, line2);
 }
