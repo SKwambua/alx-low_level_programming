ed267a09f29173cdea24fe315539bb0905fd50e5
SKwambua <samuelkikuvi33@gmail.com>
2022-07-03 11:02:45 +0000

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -92,6 +92,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	add_node(&ht->array[index], key, value);
 	if (&ht->array[index] == NULL)
-				return (0);
+		return (0);
 	return (1);
 }
